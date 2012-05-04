5f4c48624a8c3371e72e4b277a767c8515344f0c
alvaro <alvaro@alia.(none)>
2012-05-04 16:33:09 -0700

    settings

--- lodspeakr/settings.inc.php
+++ lodspeakr/settings.inc.php
@@ -2,15 +2,19 @@
 
 $conf['endpoint']['local'] = 'http://localhost:3031/poder/query';
 $conf['home'] = '/var/www/documentacion/lodspeakr/';
-$conf['basedir'] = 'http://alia/documentacion/';
+$conf['basedir'] = 'http://localhost/documentacion/';
+$lodspk['graph'] = 'http://poderopedia.com/vocab';
+
+
+
+
+### DOn't change anything below this line
+
 $conf['debug'] = false;
 
 #If you want to add/overrid a namespace, add it here
-$conf['ns']['local']   = 'http://alia/documentacion/';
+$conf['ns']['local']   = $conf['basedir'];
 $conf['ns']['poder']   = 'http://poderopedia.com/vocab/';
-
-
 $conf['mirror_external_uris'] = false;
-$lodspk['graph'] = 'http://poderopedia.com/vocab';
 $conf['root'] = 'index';
 ?>
