fdb1ddcfb896fcbcd24e6b8e4d83d1cf4fc056d8
alvaro <alvaro@alia.(none)>
2012-05-07 17:03:47 -0700

    Adding rdfs:Class to class query

--- lodspeakr/settings.inc.php
+++ lodspeakr/settings.inc.php
@@ -1,20 +1,20 @@
 <?
 
-$conf['endpoint']['local'] = 'http://localhost:3031/poder/query';
+$conf['endpoint']['local'] = 'http://localhost:3030/sparql';
 $conf['home'] = '/var/www/documentacion/lodspeakr/';
-$conf['basedir'] = 'http://localhost/documentacion/';
-$lodspk['graph'] = 'http://poderopedia.com/vocab';
+$conf['basedir'] = 'http://alia/documentacion/';
+$lodspk['graph'] = 'http://example.org/vocab';
 
 
+#If you want to add/override a namespace, add it here
+$conf['ns']['vocab']   = 'http://example.org/vocab/';
 
-
-### DOn't change anything below this line
+#############################################
+### Don't change anything below this line ###
+#############################################
 
 $conf['debug'] = false;
-
-#If you want to add/overrid a namespace, add it here
 $conf['ns']['local']   = $conf['basedir'];
-$conf['ns']['poder']   = 'http://poderopedia.com/vocab/';
 $conf['mirror_external_uris'] = false;
 $conf['root'] = 'index';
 ?>
