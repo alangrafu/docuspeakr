0289e3b53069fd61a977ea4ead3736599bcc4a27
alvaro <alvaro@alia.(none)>
2012-08-17 16:15:24 -0700

    Fixing issue #6

--- lodspeakr/default.settings.inc.php
+++ lodspeakr/default.settings.inc.php
@@ -3,11 +3,16 @@
 $conf['endpoint']['local'] = 'http://localhost:3031/sparql';
 $conf['home'] = '/var/www/docs/lodspeakr/'; #Keep the "lodspeakr/" at the end
 $conf['basedir'] = 'http://localhost/documentacion/';
+
+// If you don't have your vocabulary in a named graph, comment the following line
 $lodspk['graph'] = 'http://example.org/vocab';
 
-
-#If you want to add/override a namespace, add it here
+// If you want to add/override a namespace, add it here
 $conf['ns']['vocab']   = 'http://example.org/vocab/';
+
+// If you want to override the predicate for labels and comments, uncomment and modify the following lines
+//$lodspk['label']   = "rdfs:label";
+//$lodspk['comment'] = "rdfs:comment";
 
 #############################################
 ### Don't change anything below this line ###
