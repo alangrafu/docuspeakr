a742fe8246af4322b35c2093280def3fa3b0765d
alvaro <alvaro@alia.(none)>
2012-08-20 13:47:50 -0700

    Add ajax-based retrieval of blank node restrictions

--- lodspeakr/classes/Utils.php
+++ lodspeakr/classes/Utils.php
@@ -87,7 +87,10 @@ class Utils{
   	  	  	  $row[$k]['uri'] = 1;
   	  	  	}elseif($v['type'] == 'bnode'){
   	  	  	  $row[$k]['curie'] = 'blankNode';
+  	  	  	  $row[$k]['blank'] = 1;
   	  	  	}else{
+  	  	  	  $row[$k]['curie'] = $v['value'];
+  	  	  	  $row[$k]['literal'] = 1;
   	  	  	  if($v['datatype']){
   	  	  	    $row[$k]['type'] = $v['datatype'];
   	  	  	  }
